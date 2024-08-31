1c68d5b871a779da67364010a0393c4e8e590e3f
Rajat Pal <rajatpalbnk@gmail.com>
2024-08-31 16:50:59 +0530

    second

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <div className=' text-center p-10 min-h-screen'>
-      About page is not updated yet it will be update soon
+      About page is not updated yet it will be update soon..
     </div>
   )
 }
